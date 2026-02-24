b06571a1ade2d4e973119c5d7a8889ae9107c033
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-02-24 17:46:07 +0900

    Feat:게시판 update 기능 구현

--- BoardApplication/src/repository/PostRepository.java
+++ BoardApplication/src/repository/PostRepository.java
@@ -22,9 +22,33 @@ public class PostRepository { //DB역할
     public Post findById(int postId) {
     	return postMap.get(postId);
     }
-    
-    
+
+    //게시판 제목 변경
+    public Post updateTitle(int postId, String updatedPostTitle){
+        Post post = postMap.get(postId);
+
+        if(post!=null){
+            post.setTitle(updatedPostTitle);
+        }
+
+        return post;
+    }
+
+    //게시판 내용 변경
+    public Post updateContent(int postId, String updatedPostContent){
+        Post post = postMap.get(postId);
+
+        if(post!=null){
+            post.setContent(updatedPostContent);
+        }
+
+        return post;
+    }
+
+
     public void delete(int postId) {
     	postMap.remove(postId);
     }
+
+
 }
