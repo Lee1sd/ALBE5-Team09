b4e7f4bb13efa483d8daea8c6b1f79655a18d99e
가현 <gahyeun@gahyeon-ui-MacBookPro.local>
2026-02-24 17:06:02 +0900

    fix: src 폴더 재병합, User 로직 일부 추가

--- BoardApplication/src/repository/UserRepository.java
+++ BoardApplication/src/repository/UserRepository.java
@@ -1,5 +1,24 @@
-package repository;
+package repositroy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.User;
 
 public class UserRepository {
-
+	List<User> userList = new ArrayList<>();
+	
+	public void save(User user) {
+		userList.add(user);
+	}
+	
+	public User findByLoginId(String id) {
+		for(User u : userList) {
+			if (u.getLoginId().equals(id)) {
+				return u;
+			}
+		}
+		return null;
+	}
+	
 }
--- BoardApplication/src/runner/BoardApplication.java
+++ BoardApplication/src/runner/BoardApplication.java
@@ -2,9 +2,10 @@ package runner;
 
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        BoradConsoleRunner runner = new BoradConsoleRunner();
+        runner.boardMenu();
 
+    }
 }
